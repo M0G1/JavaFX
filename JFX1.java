3e3ed02d091c4b08b1ce33de82e193ea9e0a1033
M0G1 <44916652+M0G1@users.noreply.github.com>
2018-11-11 20:43:39 +0400

    Update JFX1.java

--- JFX1.java
+++ JFX1.java
@@ -1,36 +1,3 @@
-import javafx.application.Application;import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;  //модификация шрифта
-import javafx.scene.*;
-import javafx.scene.layout.*;
-import javafx.stage.*;
-import javafx.geometry.*;
-
-public class AlertWindow {
-    public static void display(String title, String message) {  //название окна, надпись в окне
-        Stage window = new Stage();  //новое окно новый объект
-        window.initModality(Modality.APPLICATION_MODAL);    //юлокирование пользователя с другими окнами
-        window.setTitle(title);
-        window.setMinWidth(350);    //минимальная ширина
-        window.setMinHeight(200);
-        Label label =  new Label(message);  //сообщение
-        //делаем надпись покрупнее
-        label.setFont(Font.font(20));       //размер шрифта
-
-        Button closeButton = new Button("Да");
-        closeButton.setOnAction(e ->window.close());
-        VBox layout = new VBox(20);
-        layout.getChildren().addAll(label,closeButton);
-        layout.setAlignment(Pos.CENTER);                    //по центру окна
-        Scene scene = new Scene(layout);
-        window.setScene(scene);
-        window.show();
-
-
-
-    }
-}
-
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
